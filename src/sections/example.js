f75f4815d37ffcd73005ba8fffa5f1acdc4fc831
joszze <jossecarlos@gmail.com>
2023-03-11 14:56:17 -0500

    section test

--- src/sections/example.js
+++ src/sections/example.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Image from 'next/image';
 
-// lo del fondo es una imagen por ahora sera black
+// in here here must deploy a card list 
 const Act = () => {
   return (
-    <section className="flex flex-col justify-center items-center py-20 bg-black text-white">
-      <h2 className=''>Fulfill orders accurately, every time.</h2>
+      <section className="flex  items-center py-20 text-white bg-black gap-1"> 
+          <div className='flex flex-col justify-center pl-20'>
+            <h3 className="text-5xl font-bold mr-12 leading-[62px]">
+                Fulfill orders <br /> accurately,<br /> every time.
+              </h3>
+              <ul className='list-disc flex-col'>
+                <li> Prioritize and organize orders with detailed ticket views, timers, and alerts.</li>
+                <li>Send orders to the right prep stations to help boost order accuracy.</li>
+                <li> Oversee order fulfillment and completion with Expeditor Mode.</li>
+              </ul>  
+          </div>
+
+          <div className='relative w-[600px] h-[350px]'>
+              <Image src="/example3.png" fill />
+          </div>
+        
+          
+      
     </section>
   );
 };
