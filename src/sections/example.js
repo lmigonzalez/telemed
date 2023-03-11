94cf170868804b50f034a34a04b3fc6b58d759c4
joszze <jossecarlos@gmail.com>
2023-03-11 15:19:13 -0500

    fix images

--- src/sections/example.js
+++ src/sections/example.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 // in here here must deploy a card list 
 const Act = () => {
   return (
-      <section className="flex  items-center py-20 text-white bg-black gap-1"> 
+      <section className="flex items-center py-20 text-white bg-black gap-1"> 
           <div className='flex flex-col justify-center pl-20'>
             <h3 className="text-5xl font-bold mr-12 leading-[62px]">
                 Fulfill orders <br /> accurately,<br /> every time.
@@ -16,8 +16,9 @@ const Act = () => {
               </ul>  
           </div>
 
-          <div className='relative w-[600px] h-[350px]'>
-              <Image src="/example3.png" fill />
+          <div className='flex flex-row gap-4'>
+              <Image src="/example2.png" width={500} height={500}/>              
+              <Image src="/example4.png" width={500} height={500}/>
           </div>
         
           
