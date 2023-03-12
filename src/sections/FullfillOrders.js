719ad6079939343baf4f3419db61bd40e9c83909
lmigonzalez <lmigonzalezs@gmail.com>
2023-03-12 00:38:06 -0500

    fulfill responsive

--- src/sections/FullfillOrders.js
+++ src/sections/FullfillOrders.js
@@ -8,12 +8,12 @@ import Image from 'next/image';
 // in here here must deploy a card list
 const FullfillOrders = () => {
   return (
-    <section className="relative flex text-white sm:items-center justify-between bg-black py-36 gap-20 flex-col-reverse sm:flex-row">
-      <div className="flex items-end flex-col w-1/2">
-        <div className='px-4'>
+    <section className="flex text-white md:items-center justify-between bg-white md:bg-black py-36 md:space-x-8 flex-col-reverse md:flex-row">
+      <div className="flex items-end flex-col mt-8 md:mt-0 md:w-1/2">
+        <div className='px-4 text-black md:text-white'>
           <h3 className="text-5xl font-bold leading-[62px] mb-8">
-            Fulfill orders <br /> accurately,
-            <br /> every time.
+            Fulfill orders  accurately,
+             every time.
           </h3>
           <ul className="list-disc flex-col space-y-5">
             <li className="flex">
@@ -32,7 +32,7 @@ const FullfillOrders = () => {
                 />
               </svg>
               <p>
-                Prioritize and organize orders with detailed ticket <br />{' '}
+                Prioritize and organize orders with detailed ticket {' '}
                 views, timers, and alerts.
               </p>
             </li>
@@ -52,7 +52,7 @@ const FullfillOrders = () => {
                 />
               </svg>
               <p>
-                Send orders to the right prep stations to help boost <br />{' '}
+                Send orders to the right prep stations to help boost {' '}
                 order accuracy.
               </p>
             </li>
@@ -72,19 +72,19 @@ const FullfillOrders = () => {
                 />
               </svg>
               <p>
-                Oversee order fulfillment and completion with <br /> Expeditor
+                Oversee order fulfillment and completion with  Expeditor
                 Mode.
               </p>
             </li>
           </ul>
 
         </div>
-
-
       </div>
 
 
-      <Image className='my-2 sm:my-auto' src="/kds-tickets.png" alt="kds-tickets-m" width={640} height={270} />
+      <div className='flex justify-end bg-black py-24 md:py-0 '>
+        <Image className='my-2' src="/kds-tickets.png" alt="kds-tickets-m" width={640} height={270} />
+      </div>
 
     </section >
   );
